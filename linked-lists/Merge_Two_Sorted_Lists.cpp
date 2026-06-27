cbbe19cff263b8122cf5a77444a4d21c161a0eb8
Sawmoondra <samundraaryal2@gmail.com>
2026-06-26 22:34:17 -0500

    add LC 21 Merge Two Sorted Lists

---
+++ linked-lists/Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = list1 ? list1 : list2;
+        return dummy.next;
+    }
+};
